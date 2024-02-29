f76af00102ec655557df5fe891cc74095cb4feec
아이작_조서환 <wtae1216@afreecatv.com>
2024-02-29 10:32:33 +0900

    feat: reproduce webstorm bug

---
+++ src/reproduce.ts
@@ -0,0 +1,4 @@
+import { test1 } from "@choseohwan-bug-report/webstorm-bug-report-0-0";
+import { subTest1 } from "@choseohwan-bug-report/webstorm-bug-report-0-0/sub";
+
+console.log(test1, subTest1);
